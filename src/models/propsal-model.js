15ecdd2a755add84f4d235a81ed2289f940c8431
yerobalg <yerobalg@gmail.com>
2023-05-16 20:11:22 +0700

    feat: add proposal model

---
+++ src/models/propsal-model.js
@@ -0,0 +1,68 @@
+module.exports = (sequelize, Sequelize) => {
+  const Proposal = sequelize.define(
+    'Proposal',
+    {
+      projectId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        // TODO: add reference to project table when it's ready
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      durationMonth: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      coverLetter: {
+        type: Sequelize.TEXT,
+      },
+      isApproved: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+
+      // Utility columns
+      createdBy: {
+        type: Sequelize.STRING,
+      },
+      updatedBy: {
+        type: Sequelize.STRING,
+      },
+      deletedBy: {
+        type: Sequelize.STRING,
+      },
+    },
+    {
+      paranoid: true,
+      tableName: 'proposal',
+      underscored: true,
+      timestamps: true,
+    },
+  );
+
+  Proposal.associate = (models) => {
+    Proposal.belongsTo(models.Role, {
+      foreignKey: 'roleId',
+      as: 'role',
+    });
+    Proposal.hasMany(models.UserSkill, {
+      foreignKey: 'userId',
+      as: 'userSkills',
+    });
+    Proposal.hasMany(models.UserExperience, {
+      foreignKey: 'userId',
+      as: 'userExperiences',
+    });
+    Proposal.hasMany(models.UserProject, {
+      foreignKey: 'userId',
+      as: 'userProjects',
+    });
+  };
+  return Proposal;
+};
