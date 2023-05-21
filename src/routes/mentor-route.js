81da6b1f35950334a3372a8d789ff82d77d18505
lokio <to@oopodi.com>
2023-05-21 16:06:02 +0700

    add getmentors, fixed user not found err resp code

--- src/routes/mentor-route.js
+++ src/routes/mentor-route.js
@@ -1,5 +1,6 @@
 const mentorRouter = require('express').Router();
 
+
 module.exports = class MentorRoute {
   constructor(config, log, mentorController, middleware) {
     this.config = config;
@@ -7,6 +8,23 @@ module.exports = class MentorRoute {
     this.mentorController = mentorController;
     this.authMiddleware = middleware.auth;
   }
+  /**
+   * @swagger
+   *components:
+   *  schemas:
+   *    Mentor:
+   *      type: object
+   *      properties:
+   *        id:
+   *          type: integer
+   *        userId:
+   *          type: string
+   *        price:
+   *          type: integer
+   *        durationMonth:
+   *          type: integer
+   */
+
 
   getRoutes = () => {
     mentorRouter.use(this.authMiddleware.verifyToken);
@@ -37,7 +55,24 @@ module.exports = class MentorRoute {
        *               type: string
        *               example: Mentor created
        */
-      .post('/', this.mentorController.createMentor);
+      .post('/', this.mentorController.createMentor)
+      /**
+       * @swagger
+       * /v1/mentor:
+       *   get:
+       *     tags: [Mentor]
+       *     summary: list all mentors
+       *     security:
+       *       - bearerAuth: []
+       *     responses:
+       *       200:
+       *         content:
+       *           application/json:
+       *             schema:
+       *              type: object
+       *              $ref: '#/components/schemas/Mentor'
+       */
+      .get('/', this.mentorController.getMentors);
 
     return mentorRouter;
   };
