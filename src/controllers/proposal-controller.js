c2750b5dadfc7165d01bf8431c466c069ce47955
lokio <to@oopodi.com>
2023-05-31 14:13:26 +0700

    itty bitty controller sample

--- src/controllers/proposal-controller.js
+++ src/controllers/proposal-controller.js
@@ -1,4 +1,3 @@
-const { Op } = require('sequelize');
 module.exports = class ProposalController {
   constructor(
     log,
@@ -13,6 +12,25 @@ module.exports = class ProposalController {
     this.userModel = userModel;
     this.projectModel = projectModel;
   }
+
+  proposalById = async (req, res, next) => {
+    try {
+      let userInfo;
+      userInfo = await this.userModel.findOne({
+        logging: this.log.logSqlQuery(req.context),
+        where: { id: req.params.userId },
+      });
+
+      // User registered in firebase but not in database, ignore
+      if (!userInfo) {
+        throw new ErrorLib(`user with id ${req.param.userId} not found`, 500);
+      }
+
+      this.helper.httpRespSuccess(req, res, 200, userInfo, null);
+    } catch (error) {
+      next(error);
+    }
+  };
 
 
 };
